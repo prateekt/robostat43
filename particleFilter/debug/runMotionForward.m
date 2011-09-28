89c855463ac9a3f4947556dd1e06159c68bc27ea
prateek1.tandon <prateek1.tandon@bf7babc9-86e6-54d9-2a8b-5f4565e37f6b>
2011-09-28 07:04:08 +0000

    Motion Model

---
+++ particleFilter/debug/runMotionForward.m
@@ -0,0 +1,16 @@
+function states = runMotionForward(odometry)
+
+%load odometry data
+%load parsedData.mat;
+%odometry = ROBOT{1}.Odometry;
+alpha = [0.01,0.01,0.01,0.01];
+
+%sample states
+states = zeros(length(odometry.x),3);
+x_t = [odometry.x(1),odometry.y(1),odometry.theta(1)];
+states(1,:) = x_t;
+for i=2:length(odometry.x)
+    u_t = [odometry.x(i-1),odometry.y(i-1),odometry.theta(i-1),odometry.x(i),odometry.y(i),odometry.theta(i)];
+    x_t = sample_motion_model_odometry(u_t, x_t, alpha);
+    states(i,:) = x_t;
+end
