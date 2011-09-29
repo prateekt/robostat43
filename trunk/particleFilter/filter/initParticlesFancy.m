7429994e577d402ba2393658b53f4fe41e805842
sammy.horvath@gmail.com <sammy.horvath@gmail.com@bf7babc9-86e6-54d9-2a8b-5f4565e37f6b>
2011-09-29 20:29:47 +0000

    fancy version of particle initialization

---
+++ trunk/particleFilter/filter/initParticlesFancy.m
@@ -0,0 +1,21 @@
+function [ X_0 ] = initParticlesFancy( num_part, map )
+%PARTICLEFILTER Summary of this function goes here
+%   Detailed explanation goes here
+k = 0;
+X_0 = zeros(num_part, 3);
+while k < num_part
+    a = 1;
+    b = 800;
+    x = a + (b-a).*rand(1,1);
+    y = a + (b-a).*rand(1,1);
+    a = -1 * pi;
+    b = pi;
+    theta = a + (b-a).*rand(1,1);
+    
+    if (map(round(x), round(y)) == 0)
+        k = k+1;
+        X_0(k, :) = [x y theta];
+    end
+end
+end
+
