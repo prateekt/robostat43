3167c9112477a31d86b5dc8daa07ce4f44e5ccf3
prateek1.tandon <prateek1.tandon@bf7babc9-86e6-54d9-2a8b-5f4565e37f6b>
2011-09-29 21:51:48 +0000

    Current Version

--- particleFilter/debug/playMotionForward.m
+++ particleFilter/debug/playMotionForward.m
@@ -1,6 +1,8 @@
 function playMotionForward(x,y)
 
 for i=1:length(x)
+    i
     plot(x(1:i),y(1:i),'s');
     getframe(gcf);
+    pause(0.25);
 end
--- particleFilter/filter/plotParticlesOnMap.m
+++ particleFilter/filter/plotParticlesOnMap.m
@@ -1,7 +1,11 @@
-X0 = initParticlesFancy(100, MAP);
+function plotParticlesOnMap(X0, MAP)
+
+%X0 = initParticlesFancy(100, MAP);
+figure(43);
 imagesc(MAP');
 hold on;
 %plot(X0(:,1), X0(:, 2), 'k.');
 u  = cos(X0(:,3));
 v = sin(X0(:,3));
 quiver(X0(:,1), X0(:, 2),u ,v, 0.2, 'k');
+hold off;
