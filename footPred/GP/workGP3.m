2d4b78284d7b1acb34c29c9c22e7afe6acc608b5
prateek1.tandon <prateek1.tandon@bf7babc9-86e6-54d9-2a8b-5f4565e37f6b>
2011-12-04 17:35:58 +0000

    New

--- footPred/GP/workGP3.m
+++ footPred/GP/workGP3.m
@@ -16,7 +16,7 @@ zLab_train = pos_train(2:end,3);
 %xFeatures = xLab_train;
 
 %regress features
-LOOK_AHEAD=1;
+LOOK_AHEAD=6;
 F_NUM = 2;
 ests = zeros(size(xFeatures,1),1);
 for i=1:(size(xFeatures,1)-LOOK_AHEAD)
@@ -28,7 +28,7 @@ for i=1:(size(xFeatures,1)-LOOK_AHEAD)
     m = regress(x_in,y_in,z_in,1);
 
     %store estimate
-    ests(i+1) = m;
+    ests(i+LOOK_AHEAD) = m;
     
     i
 end
