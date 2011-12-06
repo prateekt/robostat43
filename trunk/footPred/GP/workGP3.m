f1589f31bf819f8ec794c6619a62ebf8a0f1c6ae
prateek1.tandon <prateek1.tandon@bf7babc9-86e6-54d9-2a8b-5f4565e37f6b>
2011-12-06 07:04:44 +0000

    xFeatures

--- trunk/footPred/GP/workGP3.m
+++ trunk/footPred/GP/workGP3.m
@@ -13,7 +13,7 @@ xLab_train = pos_train(2:end,1);
 yLab_train = pos_train(2:end,2);
 zLab_train = pos_train(2:end,3);
 
-xFeatures = zLab_train;
+xFeatures = xLab_train;
 
 %regress features
 LOOK_AHEAD=101;
